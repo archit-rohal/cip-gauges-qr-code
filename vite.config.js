bc50455d7dc84e0b18490a1f2b951cb0bd59e9de
Archit Rohal <rohalarchit@gmail.com>
2026-03-14 18:03:14 +0530

    Comment out base path in Vite configuration for flexibility in deployment

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import react from '@vitejs/plugin-react'
  */
 export default defineConfig({
   plugins: [react()],
-  base: '/cip-gauges-qr-code/',
+  //base: '/cip-gauges-qr-code/',
   build: {
     outDir: 'dist',
     sourcemap: false,
